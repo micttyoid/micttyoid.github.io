f793b402515c44fb03582e0a9c2f91192d6e9482
Luke Yoo <w.lukeyoo@gmail.com>
2025-02-11 00:15:30 -0800

    Add game/flappy

---
+++ game/flappy/flappy_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h977272c8ebe96f74: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9413383f45f463e5: (a: number, b: number) => void;
+export const closure2954_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2952_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8c80d2c0a9dc670e: (a: number, b: number) => void;
+export const closure99711_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
